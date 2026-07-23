b088264ed20fc4a96d354735b4bbe5efc41ba95a
Yash <yash230505@gmail.com>
2026-07-23 23:12:36 +0530

    Day 6: Architected API storage by extracting JavaScript array into dedicated data module

---
+++ data.js
@@ -0,0 +1,5 @@
+// In-memory array to store our blog posts
+// Extracted to a separate module for better code architecture
+const blogs = [];
+
+module.exports = blogs;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const blogs = require('./data.js');
 const app = express();
 const port = 3000;
 
@@ -10,8 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// In-memory array to temporarily store our blog posts
-let blogs = [];
 
 // --- API ROUTES ---
 
